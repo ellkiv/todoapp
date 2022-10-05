d7af9a6e51f9442e0801d72672a966abab4ff146
Ella Annika <ellkiv@gmail.com>
2022-10-05 10:40:04 +0300

    rakennetta muutettu

--- todocomponents/src/App.js
+++ todocomponents/src/App.js
@@ -1,37 +1,13 @@
 import './App.css';
-import React, { useState } from 'react';
-import ToDoTable from './components/ToDoTable'
-import ToDoForm from './components/ToDoList'
+import React from 'react';
+import ToDoList from './components/ToDoList'
 
 function App() {
-  const [toDo, setToDo] = useState({date:'', desc:''});
-  const [todos, setTodos] = useState([]);
-
-  const inputChanged = (event) => {
-    setToDo({...toDo, [event.target.name]: event.target.value});
-  }
-
-  const addTodo = (event) => {
-    event.preventDefault();
-    setTodos([...todos, toDo]);
-    //console.log(todos);
-    //lomakekentän tyhjennys
-    setToDo({date:'', desc:''});
-  }
-
-  function deleteToDo(index) {
-    const removeItem = todos.filter((toDo, i) => {
-     return i !== index;
-    });
-    setTodos(removeItem);
-    console.log(index);
-  }
 
   return (
     <div className="App">
       <h1>ToDoList</h1>
-      <ToDoForm toDo={toDo} inputChanged={inputChanged} addTodo={addTodo} />
-      <ToDoTable todos={todos} deleteToDo={deleteToDo} />
+      <ToDoList />
     </div>
   );
 }
--- todocomponents/src/components/ToDoList.js
+++ todocomponents/src/components/ToDoList.js
@@ -1,18 +1,41 @@
 import React, { useState } from 'react';
+import ToDoTable from './ToDoTable'
 
-export default function ToDoList(props){
+export default function ToDoList(){
+    const [toDo, setToDo] = useState({date:'', desc:''});
+    const [todos, setTodos] = useState([]);
+  
+    const inputChanged = (event) => {
+      setToDo({...toDo, [event.target.name]: event.target.value});
+    }
+  
+    const addTodo = (event) => {
+      event.preventDefault();
+      setTodos([...todos, toDo]);
+      //console.log(todos);
+      //lomakekentän tyhjennys
+      setToDo({date:'', desc:''});
+    }
+  
+    function deleteToDo(index) {
+      const removeItem = todos.filter((toDo, i) => {
+       return i !== index;
+      });
+      setTodos(removeItem);
+      console.log(index);
+    }
 
     return(
         <div>
         <form>
         <h4>Add ToDo:</h4>
             <label>Description: </label>
-            <input type="text" name="desc" value={props.toDo.desc} onChange={props.inputChanged}/>
+            <input type="text" name="desc" value={toDo.desc} onChange={inputChanged}/>
             <label>Date: </label>
-            <input type="text" name="date" value={props.toDo.date} onChange={props.inputChanged}/>
-            <button onClick={props.addTodo}>Add</button>
+            <input type="text" name="date" value={toDo.date} onChange={inputChanged}/>
+            <button onClick={addTodo}>Add</button>
         </form>
-
+        <ToDoTable todos={todos} deleteToDo={deleteToDo} />
         </div>
     );
 }
